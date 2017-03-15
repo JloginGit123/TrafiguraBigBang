e7164e02cb1fa6065c589bd914c5d81d64d36e2e
Hari G <hari.gurunathan@cognizant.com>
2017-03-15 13:17:22 +0530

    IssueID::UST26 OwnerId::1000141208 entityType::AUSRST Status::Done Reviewer::Hari Comments::Cheked in from visual studio 2013

--- TrafWebsite/TrafWebsite/HomePage.aspx.cs
+++ TrafWebsite/TrafWebsite/HomePage.aspx.cs
@@ -11,7 +11,7 @@ namespace TrafWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Test123--update-defect fixed
+            //Test123--update-for UST 26
            
         }
     }
